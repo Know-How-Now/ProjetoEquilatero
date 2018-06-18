1cb15eedcd5c722ff1c2a93d5cb58de8ae0e305e
Leonardo Castelo Branco de Melo <cbmelo97@gmail.com>
2018-06-18 20:23:29 -0300

    Update sample_raw_data.py

--- Beta Database/sample_raw_data.py
+++ Beta Database/sample_raw_data.py
@@ -5,7 +5,7 @@ import custom_classes as c_class
 import query_functions as query
 from tqdm import tqdm
 from faker import Faker
-fake = Faker()
+fake = Faker('pt_BR')
 
 #---------------------[ SPONSOR SAMPLE DATA ]--------------------#
     #---[ sponsor_id = range(10001,20000) // ex.: 12345 ]---#
@@ -17,7 +17,7 @@ def sample_sponsor_data(sample_data_ammount):
     data_created = 0
     pbar = tqdm(total=sample_data_ammount)
     while data_created < sample_data_ammount:
-        id = str(random.randint(10001,20000))
+        id = '{}'.format(int(query.get_collection_docs('Sponsors')[-1])+1).translate({ord(char): None for char in "'[]'"})
         if query.check_data_existance('Sponsors',id) == True:
             continue
         else:
@@ -101,32 +101,80 @@ def sample_sensor_data(sample_data_ammount):
     print("\nSuccesfully sent all {} 'Sensor' data samples to the Database!".format(sample_data_ammount))
 #---------------------------------------------------------------------#
 
-#---------------------[ USER SAMPLE DATA ]---------------------#
-        #---[ user_id = range(60 ++) // ex.: 9999 ]---#
-          #---[ name, sex, age=0, contact='' ]---#
+#---------------------[ PROFILE SAMPLE DATA ]---------------------#
+#---[ profile_id = 5 + deficiência {range(0,6)} + sensor_id // ex.: 50+28101001 ]---#
+            #--------[ sex, birthdate, profession, transport, motivation ]--------#              
 
-# [Function Start: Set/Update 'User' with generated data] 
-def sample_user_data(sample_data_ammount):
-    print("Creating sample 'User' data...")
+# [Function Start: Sample data FUNC for 'Profile'] 
+def random_profile_data():
+    sex = random.choice(['Male','Female'])
+    birthdate = fake.date_time_between(start_date='-50y', end_date='-15y') #date only - not timestamp - FIX
+    profession = fake.job()
+    transport = random.choice(['Carro','Onibus','Van','Bicicleta','Outro'])
+    motivation = random.choice(['Motivo1','Motivo2','Motivo3','Motivo4','Motivo4'])
+    data = c_class.Profile(sex,birthdate,profession,transport,motivation).to_dict()
+    return data
+
+# [Function Start: Set/Update 'Profile' with generated data] 
+def sample_profile_data(sample_data_ammount):
+    print("Creating sample 'Profile' data...")
     data_created = 0
     pbar = tqdm(total=sample_data_ammount)
     while data_created < sample_data_ammount:
-        id = '{}'.format(random.randint(60,9999))
-        if query.check_data_existance('Users',id) == True:
+        random_sensor_id = random.choice(query.get_collection_docs('Sensors'))
+        id = '5{}{}'.format(random.randint(0,7),random_sensor_id).translate({ord(char): None for char in "'[]'"})
+        if query.check_data_existance('Profiles',id) == True:
             continue
         else:
-            name = fake.name()
-            sex = random.choice(['Male','Female'])
-            birthdate = fake.date_time_between(start_date='-50y', end_date='-15y') #date only - not timestamp - FIX
-            contact = fake.safe_email()
-            user_ref = query.db.collection('Users').document(id)
-            user_ref.set(c_class.User(name, sex, birthdate, contact).to_dict())
-            pbar.update(+1)
+            profile_ref = query.db.collection('Profiles').document(id)
+            profile_ref.set(c_class.Timestamp(random_profile_data()).to_dict())
+            i = 1
+            while i < 5:
+                profile_ref.update(c_class.Timestamp(random_profile_data()).to_dict())
+                pbar.update(+(1/4))
+                i = i + 1
             data_created = data_created + 1
-    print("\nSuccesfully sent all {} 'User' data samples to the Database!".format(sample_data_ammount))
+    print("\nSuccesfully sent all {} 'Profile' data samples to the Database!".format(sample_data_ammount))
 #---------------------------------------------------------------------#
 
-#sample_sponsor_data(10)
+#---------------------[ INVESTMENT SAMPLE DATA ]---------------------#
+#---[ investment_id = sponsor_id + item_id // ex.: 1000128101 ]---#
+        #---[ ammount=0, allocated_to=[''], ebtida=[0] ]---# 
+
+# [Function Start: Sample data FUNC for 'Investment'] 
+def random_investment_data():
+    ammount = random.randint(2001,10000)
+    allocated_to = fake.words(6) #create dict;
+    k = random.random()
+    ebtida = ammount + (ammount*k)
+    data = c_class.Investment(ammount,allocated_to,ebtida).to_dict()
+    return data
+
+# [Function Start: Set/Update 'Investor' with generated data] 
+def sample_investment_data(sample_data_ammount):
+    print("Creating sample 'Investment' data...")
+    data_created = 0
+    pbar = tqdm(total=sample_data_ammount)
+    while data_created < sample_data_ammount:
+        random_sponsor_id = random.choice(query.get_collection_docs('Sponsors'))
+        random_item_id = random.choice([random.choice(query.get_collection_docs('Tracks')), random.choice(query.get_collection_docs('Sensors')), random.randint(10001,99999)]) # add func to get control ids
+        id = '{}{}'.format(random_sponsor_id, random_item_id)
+        if query.check_data_existance('Investments',id) == True:
+            continue
+        else:
+            investment_ref = query.db.collection('Investments').document(id)
+            investment_ref.set(c_class.Timestamp(random_investment_data()).to_dict())
+            i = 1
+            while i < 5:
+                investment_ref.update(c_class.Timestamp(random_investment_data()).to_dict())
+                pbar.update(+(1/4))
+                i = i + 1
+            data_created = data_created + 1
+    print("\nSuccesfully sent all {} 'Investment' data samples to the Database!".format(sample_data_ammount))
+#---------------------------------------------------------------------#
+
+#sample_sponsor_data(20)
 #sample_track_data(10)
-#sample_user_data(20)
 #sample_sensor_data(50)
+#sample_profile_data(20)
+#sample_investment_data(10)
